60165de5e3553dcaff2957d1d1b4e2e78f942c52
SheshamJoseph <sheshamabraham@gmail.com>
2024-12-06 11:40:53 +0100

    modified Task struct; added functions, Status enum

--- TaskTracker/TaskTracker.h
+++ TaskTracker/TaskTracker.h
@@ -1,26 +1,37 @@
 #ifndef TASK_TRACKER_H
 #define TASK_TRACKER_H
 
+#include <string>
 #include <ctime>
+
+enum Status{
+    done,
+    in_progress,
+    todo,
+};
 
 struct Task {
     int id {};
     std::string description {};
-    std::string status {};
-    std::time_t createdAt {};
-    std::time_t modifiedAt {};
+    Status status {};    
+    std::string createdAt {};
+    std::string modifiedAt {};
 };
 
 void displayHelp();
 
-void addTask(std::string_view description);
-void updateTask(int id, std::string_view description);
+void addTask(const std::string& description);
+void updateTask(int id, const std::string& description);
 void deleteTask(int id);
 
 void markAsDone(int id);
 void markAsInProgress(int id);
 
 void listTasks();
-void listTasksByStatus(std::string_view flag="all");
+void listTasksByStatus(Status status);
+
+std::string formatTime(std::time_t time);
+std::string statusToString(Status status);
+Status stringToStatus(const std::string& status);
 
 #endif // TASK_TRACKER_H
